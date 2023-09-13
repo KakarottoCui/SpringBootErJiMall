5d7bfc6decd3946150595027bd59cae9fc44f739
cuifengbo <471363336@qq.com>
2023-09-13 09:44:59 +0800

    1

---
+++ src/main/java/ltd/newbee/mall/NewBeeMallApplication.java
@@ -0,0 +1,10 @@
+
+package ltd.newbee.mall;
+
+import org.mybatis.spring.annotation.MapperScan;
+
+public class NewBeeMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewBeeMallApplication.class, args);
+    }
+}
---
+++ src/main/java/ltd/newbee/mall/controller/admin/AdminController.java
@@ -0,0 +1,24 @@
+
+package ltd.newbee.mall.controller.admin;
+
+import ltd.newbee.mall.common.ServiceResultEnum;
+import ltd.newbee.mall.entity.AdminUser;
+import ltd.newbee.mall.service.AdminUserService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+
+
+
+
+ */
+@
+    }
+}
